0c32c23648a214667f954bdf74ce7e0b92b285f7
Ali Adeku <aliadeku.aam@gmail.com>
2025-07-22 23:40:36 +0100

    update subscription repository

--- src/modules/subscription/subscription.repository.ts
+++ src/modules/subscription/subscription.repository.ts
@@ -10,16 +10,4 @@ export class SubsscriptionRepository extends Repository<Subscription> {
   constructor(datasource: DataSource) {
     super(Subscription, datasource.createEntityManager());
   }
-
-  //   async findUserByEmailOrusername(identifier: string): Promise<User | null> {
-  //     return this.findOne({
-  //       where: [{ email: identifier }],
-  //     });
-  //   }
-
-  //   async findUserById(userId: string): Promise<User | null> {
-  //     return this.findOne({ where: { id: userId } });
-  //   }
-
-  async createSub(subData: CreateSub): Promise<Subscription> {}
 }
